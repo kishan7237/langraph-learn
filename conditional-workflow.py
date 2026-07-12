4d332a1380ee982cef066e2b7a8b5c1ace6f81d7
kishan7237 <61774722+kishan7237@users.noreply.github.com>
2026-07-12 23:44:33 +0530

    conditional workflow

--- conditional-workflow.py
+++ conditional-workflow.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel, Field
 
 
 # Use a model that supports structured output
-model = ChatOllama(model="llama3.2")
+model = ChatOllama(model="tinyllama:latest")
 
 
 # -----------------------------
@@ -122,9 +122,7 @@ def negative_response(state: ReviewState):
 # -----------------------------
 # Router
 # -----------------------------
-def check_sentiment(
-    state: ReviewState,
-) -> Literal["positive_response", "run_diagnosis"]:
+def check_sentiment(state: ReviewState) -> Literal["positive_response", "run_diagnosis"]:
     if state["sentiment"] == "positive":
         return "positive_response"
     else:
@@ -142,12 +140,7 @@ graph.add_node("run_diagnosis", run_diagnosis)
 graph.add_node("negative_response", negative_response)
 
 graph.add_edge(START, "find_sentiment")
-
-graph.add_conditional_edges(
-    "find_sentiment",
-    check_sentiment,
-)
-
+graph.add_conditional_edges("find_sentiment",check_sentiment)
 graph.add_edge("run_diagnosis", "negative_response")
 graph.add_edge("negative_response", END)
 graph.add_edge("positive_response", END)
