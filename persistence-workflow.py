00f300cba5b242cd58c7a5901adace996a2e3458
kishan7237 <61774722+kishan7237@users.noreply.github.com>
2026-07-20 01:21:48 +0530

    persistence in langgraph

--- persistence-workflow.py
+++ persistence-workflow.py
@@ -10,17 +10,23 @@ class JokeState(TypedDict):
     joke:str
     explanation:str
 
-def generate_joke(state:JokeState):
+def generate_joke(state:JokeState)-> dict:
     prompt = f"""
-        generate a joke on the topic: {state['topic']}
+        You are a comedian.
+        
+        Generate one short joke about:
+        
+        Topic: {state['topic']}
     """
     response = llm.invoke(prompt).content
 
     return {'joke':response}
 
-def generate_explanation(state:JokeState):
+def generate_explanation(state:JokeState)-> dict:
     prompt = f"""
-        write an explanation for the joke: {state['joke']}
+        Explain why the following joke is funny.
+        
+        Joke:{state['joke']}
     """
     response = llm.invoke(prompt).content
 
@@ -42,11 +48,15 @@ workflow = graph.compile(checkpointer=checkpointer)
 config1 = {"configurable": {"thread_id":"1"}}
 while True:
     user_input = input("\nEnter Joke topic:")
-    if user_input == "exit" or user_input == "quit":
+    if user_input.lower() in ("exit", "quit"):
+        print("Goodbye!")
         break
+    if not user_input:
+        print("Please enter a topic.")
+        continue
     initial_state = {'topic':user_input}
     final_state = workflow.invoke(initial_state, config=config1)
-
+    print(list(workflow.get_state_history(config1)))
     print(final_state['joke'])
     print(final_state['explanation'])
 
