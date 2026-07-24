e44b3a4b2ab6d57752fe4e1f5ef397b62792481d
kishan7237 <61774722+kishan7237@users.noreply.github.com>
2026-07-24 19:49:56 +0530

    chatbot with streaming functionality

---
+++ chat-agent-with-streamming/streamlit_frontend.py
@@ -0,0 +1,56 @@
+import streamlit as st
+from langchain_core.messages import HumanMessage
+from chatbot_workflow import workflows,llm
+
+CONFIG = {'configurable': {'thread_id': 'thread-1'}}
+# st.session_state -> dist->
+
+if 'message_history' not in st.session_state:
+    st.session_state['message_history'] = []
+
+# Loading the conversation history
+for message in st.session_state['message_history']:
+    with st.chat_message(message['role']):
+        st.text(message['content'])
+
+user_input = st.chat_input('Type your message...')
+
+if user_input:
+    # First add the message to message_history
+    st.session_state['message_history'].append({'role': 'user', 'content': user_input})
+    with st.chat_message('user'):
+        st.text(user_input)
+
+    # this part for chatbot with streaming
+    with st.chat_message('assistant'):
+        ai_message = st.write_stream(
+            message_chunk.content for message_chunk, metadata in workflows.stream(
+                {'messages': [HumanMessage(content=user_input)]},
+                config=CONFIG,
+                stream_mode='messages'
+            )
+        )
+
+        st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
+
+# ==========================================ui=======================================
+# ----------------------------
+# Page Config
+# ----------------------------
+st.set_page_config(
+    page_title="Chat Agent",
+    page_icon="🤖",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+# ----------------------------
+# Sidebar
+# ----------------------------
+with st.sidebar:
+    st.image(
+        "https://cdn-icons-png.flaticon.com/512/4712/4712109.png",
+        width=100
+    )
+    st.title("🤖 Chat Agent")
+    st.divider()
